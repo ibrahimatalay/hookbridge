59b4068dd297030ede9b41b7b6d164d2dd4db464
ibrahimatalay <ibrahimatalay721@gmail.com>
2026-06-21 16:40:53 +0300

    add endpoint store with form request validation and api resource

--- app/Http/Controllers/EndpointController.php
+++ app/Http/Controllers/EndpointController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 use App\Models\Tenant;
+use App\Http\Requests\StoreEndpointRequest;
+use App\Http\Resources\EndpointResource;
 
 class EndpointController extends Controller
 {
     public function index(Tenant $tenant)
     {
-        return response()->json($tenant->endpoints);
+        return EndpointResource::collection($tenant->endpoints);
+    }
+
+    public function store(StoreEndpointRequest $request, Tenant $tenant)
+    {
+        $endpoint = $tenant->endpoints()->create($request->validated());
+
+        return (new EndpointResource($endpoint))
+            ->response()
+            ->setStatusCode(201);
     }
 }
